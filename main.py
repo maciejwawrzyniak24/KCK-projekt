bb37aac0b1638d3172c5a6752cba10e5d08bbfc6
Maciej Wawrzyniak <maciej.s.wawrzyniak@student.put.poznan.pl>
2021-12-05 13:37:42 +0100

    dodalem opcje zeby pojawialo sie wiecej linii, ale nie do konca dziala jak powinna

--- main.py
+++ main.py
@@ -18,63 +18,69 @@ def main():
     d = {}
     d[0] = "./zdjecia/1.jpg"
     d[1] = "./zdjecia/2.jpg"
-    #d[2] = "./zdjecia/deska-0x400-ffffff.jpg"
-    #d[3] = "./zdjecia/262107721_597105391558839_4796743744082034723_n.jpg"
-    #d[4] = "./zdjecia/dd.jpg"
-    #d[5] = "./zdjecia/dd2.jpg"
+    d[2] = "./zdjecia/deska-0x400-ffffff.jpg"
+    d[3] = "./zdjecia/262107721_597105391558839_4796743744082034723_n.jpg"
+    d[4] = "./zdjecia/dd.jpg"
+    d[5] = "./zdjecia/dd2.jpg"
 
     for i in range(len(d)):
         img = cv2.imread(d[i])
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         #cv2.imwrite("s{}.jpg".format(i), gray)
 
-        ret,thresh1 = cv2.threshold(gray,80,255,cv2.THRESH_BINARY)
-        #cv2.imwrite("s{}1.jpg".format(i), thresh1)
+        #gray = filters.rank.median(gray, np.ones([3,3]))
 
-        imgEnd = cv2.Canny(thresh1,90,150,apertureSize = 3)
+        ret,thresh1 = cv2.threshold(gray,80,255,cv2.THRESH_BINARY)
+        #cv2.imwrite("a{}1.jpg".format(i), thresh1)
+
+        imgEnd = cv2.Canny(thresh1,50,150,apertureSize = 3)
         #cv2.imwrite("s{}2.jpg".format(i), imgEnd)
 
         kernel = np.ones((2,2),np.uint8)
         dil = cv2.dilate(imgEnd,kernel,iterations = 1)
-        #cv2.imwrite("s{}3.jpg".format(i), dil)
+        #cv2.imwrite("d{}3.jpg".format(i), dil)
 
-        
-        edges = cv2.Canny(gray,50,150,apertureSize = 3)
+        #edges = cv2.Canny(gray,50,150,apertureSize = 3)
         #cv2.imwrite("s{}35.jpg".format(i), edges)
 
         lines = cv2.HoughLines(dil,1,np.pi/180,250)
-        print(len(lines))
+        #print(len(lines))
+
+        if(len(lines) < 20):
+            imgEnd = cv2.Canny(gray, 50, 150, apertureSize = 3)
+            dil = cv2.dilate(imgEnd, kernel, iterations = 1)
+            lines = cv2.HoughLines(dil, 1, np.pi/180, 200)
 
         imgCopy = img.copy()
 
         deleted = []
-        for i in range(len(lines)):
-            for j in range(i + 1, len(lines)):
-                rho1, theta1, = lines[i][0]
-                rho2, theta2, = lines[j][0]
-                if(abs(rho1 - rho2) < 13 and abs(theta1 - theta2) < 0.02):
-                    deleted.append(j)
+        for deli in range(len(lines)):
+            for delj in range(deli + 1, len(lines)):
+                rho1, theta1, = lines[deli][0]
+                rho2, theta2, = lines[delj][0]
+                if(abs(rho1 - rho2) < 15 and abs(theta1 - theta2) < 0.05):
+                    deleted.append(delj)
         deleted = list(set(deleted))
         deleted.sort(reverse=True)
-        for i in deleted:
-            lines = np.delete(lines,i, axis=0)
+        for deli in deleted:
+            lines = np.delete(lines,deli, axis=0)
 
+        j = 0
         for line in lines:
             j+=1
             rho, theta = line[0]
-            if(1.40 < theta < 1.60):
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a*rho
-                y0 = b*rho
-                x1 = int(x0 + 2000*(-b))
-                y1 = int(y0 + 2000*(a))
-                x2 = int(x0 - 2000*(-b))
-                y2 = int(y0 - 2000*(a))
-                cv2.line(imgCopy,(x1,y1),(x2,y2),(0,0,255),2)
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 2000*(-b))
+            y1 = int(y0 + 2000*(a))
+            x2 = int(x0 - 2000*(-b))
+            y2 = int(y0 - 2000*(a))
+            cv2.line(imgCopy,(x1,y1),(x2,y2),(0,0,255),2)
 
-        print("obraz {}, linii {}".format(i,j))
-        cv2.imwrite("s{}4.jpg".format(i), imgCopy) #ostateczny obraz
+        #print("obraz {}, linii {}".format(i,j))
+        #cv2.imwrite("s{}4.jpg".format(i), imgCopy) #ostateczny obraz
 
         poziome = np.empty((int(len(lines)/2),1,2), np.float32)
         pionowe = np.empty((int(len(lines)/2),1,2), np.float32)
@@ -88,7 +94,6 @@ def main():
             else:
                 pionowe[k] = lines[i]
                 k += 1
-
         
         imgCopy = img.copy()
         for line1 in poziome:
@@ -111,8 +116,8 @@ def main():
                 y4 = int(y3 + 2000*(a2))
                 x5 = int(x3 - 2000*(-b2))
                 y5 = int(y3 - 2000*(a2))
-                d = findIntersection(x1,y1,x2,y2,x4,y4,x5,y5)
-                cv2.circle(imgCopy, (int(d[0]), int(d[1])), radius=2, color=(255,0,0), thickness=-1)
+                middle_of_circle = findIntersection(x1,y1,x2,y2,x4,y4,x5,y5)
+                cv2.circle(imgCopy, (int(middle_of_circle[0]), int(middle_of_circle[1])), radius=2, color=(255,0,0), thickness=-1)
                
         cv2.imwrite("s{}5.jpg".format(i), imgCopy) #ostateczny obraz 
 
