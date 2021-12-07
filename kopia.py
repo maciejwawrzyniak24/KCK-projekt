7d1d1a9baf309153ca0b6e9eb148346ec4eaec63
Maciej Wawrzyniak <maciej.s.wawrzyniak@student.put.poznan.pl>
2021-12-07 14:12:00 +0100

    dzialajaca wersja

--- kopia.py
+++ kopia.py
@@ -14,25 +14,25 @@ def findIntersection(x1,y1,x2,y2,x3,y3,x4,y4):
         return [px, py]
 
 
-
-
-
 def main():
     d = {}
     d[0] = "./zdjecia/1.jpg"
-    d[1] = "./zdjecia/262107721_597105391558839_4796743744082034723_n.jpg"
-    #d[1] = "./zdjecia/2.jpg"
-    #d[2] = "./zdjecia/deska-0x400-ffffff.jpg"
-    #d[3] = "./zdjecia/262107721_597105391558839_4796743744082034723_n.jpg"
-    #d[4] = "./zdjecia/dd.jpg"
-    #d[5] = "./zdjecia/dd2.jpg"
+    d[1] = "./zdjecia/grr.jpg"
+    d[2] = "./zdjecia/fff.jpg"
+    d[3] = "./zdjecia/grrr.jpg"
+    d[4] = "./zdjecia/44.jpg"
+    d[5] = "./zdjecia/beuken-vineer-9x9-2-.jpg"
+    d[6] = "./zdjecia/dd2.jpg"
+    d[7] = "./zdjecia/33.jpg"
+    d[8] = "./zdjecia/rr.jpg"
+    d[9] = "./zdjecia/rr2.jpg"
+    d[10] = "./zdjecia/tt1.jpg"
+    d[11] = "./zdjecia/tt2.jpg"
 
     for i in range(len(d)):
         img = cv2.imread(d[i])
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         #cv2.imwrite("s{}.jpg".format(i), gray)
-
-        #gray = filters.rank.median(gray, np.ones([3,3]))
 
         ret,thresh1 = cv2.threshold(gray,80,255,cv2.THRESH_BINARY)
         #cv2.imwrite("a{}1.jpg".format(i), thresh1)
@@ -44,16 +44,16 @@ def main():
         dil = cv2.dilate(imgEnd,kernel,iterations = 1)
         #cv2.imwrite("d{}3.jpg".format(i), dil)
 
-        #edges = cv2.Canny(gray,50,150,apertureSize = 3)
-        #cv2.imwrite("s{}35.jpg".format(i), edges)
-
         lines = cv2.HoughLines(dil,1,np.pi/180,250)
-        #print(len(lines))
+        if (lines is not None):
+            print(len(lines))
 
-        if(len(lines) < 20):
+        if(lines is None or len(lines) < 9):
+
             imgEnd = cv2.Canny(gray, 50, 150, apertureSize = 3)
             dil = cv2.dilate(imgEnd, kernel, iterations = 1)
-            lines = cv2.HoughLines(dil, 1, np.pi/180, 200)
+            lines = cv2.HoughLines(dil, 1, np.pi/180, 400)
+            
 
         imgCopy = img.copy()
 
@@ -69,35 +69,31 @@ def main():
         for deli in deleted:
             lines = np.delete(lines,deli, axis=0)
 
-        j = 0
-        for line in lines:
-            j+=1
-            rho, theta = line[0]
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + 2000*(-b))
-            y1 = int(y0 + 2000*(a))
-            x2 = int(x0 - 2000*(-b))
-            y2 = int(y0 - 2000*(a))
-            cv2.line(imgCopy,(x1,y1),(x2,y2),(0,0,255),2)
+        #j = 0
+        #for line in lines:
+        #    j+=1
+        #    rho, theta = line[0]
+        #    a = np.cos(theta)
+        #    b = np.sin(theta)
+        #    x0 = a*rho
+        #    y0 = b*rho
+        #    x1 = int(x0 + 2000*(-b))
+        #    y1 = int(y0 + 2000*(a))
+        #    x2 = int(x0 - 2000*(-b))
+        #    y2 = int(y0 - 2000*(a))
+        #    cv2.line(imgCopy,(x1,y1),(x2,y2),(0,0,255),2)
 
         #print("obraz {}, linii {}".format(i,j))
         #cv2.imwrite("s{}4.jpg".format(i), imgCopy) #ostateczny obraz
 
-        poziome = np.empty((int(len(lines)/2),1,2), np.float32)
-        pionowe = np.empty((int(len(lines)/2),1,2), np.float32)
-        j = 0
-        k = 0
-        for i in range(len(lines)):
-            rho, theta = lines[i][0]
+        poziome = np.copy(lines)
+        pionowe = np.copy(lines)
+        for ii in reversed(range(len(lines))):
+            rho, theta = lines[ii][0]
             if(1.40 < theta < 1.60):
-                poziome[j] = lines[i]
-                j += 1
+                pionowe = np.delete(pionowe, ii, axis=0)
             else:
-                pionowe[k] = lines[i]
-                k += 1
+                poziome = np.delete(poziome, ii, axis=0)
         
         circles = np.empty((len(poziome) * len(pionowe), 2), dtype=np.float32)
 
